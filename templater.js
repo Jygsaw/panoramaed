fa99776aed186ee90518510791124bebe9974280
Justin Cheung <jygsaw@gmail.com>
2014-09-17 16:06:35 -0700

    Implement token substitution

--- templater.js
+++ templater.js
@@ -1,32 +1,34 @@
-// initializing required libraries
+// initialize libraries
 var fs = require('fs');
 
-// initializing variables
-var templateFilename = process.argv[2];
-var dataFilename = process.argv[3];
-var outputFilename = process.argv[4];
+// initialize data, read, and write streams
+var data = JSON.parse(fs.readFileSync(process.argv[3]));
+var readStream = fs.createReadStream(process.argv[2]);
+var writeStream = fs.createWriteStream(process.argv[4]);
 
-// TODO remove debugging and collapse variable names
-console.log("Executing Templater");
-console.log("  template:", templateFilename);
-console.log("  data:", dataFilename);
-console.log("  output:", outputFilename);
+// map data token to data value
+function lookup(data, path) {
+  var parts = path.split('.').reverse();
+  var result = data;
+  while (parts.length) {
+    result = result[parts.pop()];
+  }
+  return result;
+}
 
-// read and initialize data
-var data = JSON.parse(fs.readFileSync(dataFilename));
-
-// initialize read and write streams
-var readStream = fs.createReadStream(templateFilename);
-var writeStream = fs.createWriteStream(outputFilename);
-
-// process incoming file
-readStream.on('readable', processFile);
-
+// process input file line by line
 function processFile() {
-  while(line = readStream.read()) {
-    // if (line) {
-    //   console.log(line.toString());      
-    // }
+  while (line = readStream.read()) {
+    line = line.toString();
+    var tokenRegex = /<\*\s*(.+?)\s*\*>/;
+    var match;
+    while (match = line.match(tokenRegex)) {
+      // substitute data token with data value
+      line = line.replace(tokenRegex, lookup(data, match[1]));
+    }
     writeStream.write(line);
   }
 }
+
+// process template
+readStream.on('readable', processFile);
