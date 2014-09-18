bd228bf16e62890238b7b406098fd4b6639fe10b
Justin Cheung <jygsaw@gmail.com>
2014-09-17 23:11:10 -0700

    Modify templater to process template file line-by-line

--- templater.js
+++ templater.js
@@ -1,10 +1,13 @@
 // initialize libraries
 var fs = require('fs');
 
-// initialize data, read, and write streams
+// initialize variables
 var data = JSON.parse(fs.readFileSync(process.argv[3]));
 var readStream = fs.createReadStream(process.argv[2]);
 var writeStream = fs.createWriteStream(process.argv[4]);
+var tokenLogic = {
+  EACH: function() { return 'stuff'; },
+};
 
 // map data token to data value
 function lookup(data, path) {
@@ -17,18 +20,33 @@ function lookup(data, path) {
 }
 
 // process input file line by line
-function processFile() {
-  while (line = readStream.read()) {
-    line = line.toString();
-    var tokenRegex = /<\*\s*(.+?)\s*\*>/;
+function processLines(lines) {
+  var result = [];
+  var line;
+  while (line = lines.pop()) {
+    var tokenRegex = /<\*\s*(\S*)?\s*?(.+?)\s*\*>/;
     var match;
     while (match = line.match(tokenRegex)) {
-      // substitute data token with data value
-      line = line.replace(tokenRegex, lookup(data, match[1]));
+console.log("logic key:", match[1]);
+console.log("data key:", match[2]);
+      if (tokenLogic[match[1]]) {
+        // collect lines and perform token logic
+console.log("token logic detected!!!");
+        line = tokenLogic[match[1]]();
+      } else {
+        // substitute data token with data value
+        line = line.replace(tokenRegex, lookup(data, match[1]));
+      }
     }
-    writeStream.write(line);
+    result.push(line);
   }
+  return result;
 }
 
 // process template
-readStream.on('readable', processFile);
+readStream.on('readable', function() {
+  var lineStack = readStream.read().toString().split('\r\n').reverse();
+  processLines(lineStack).forEach(function(data) {
+    writeStream.write(data + '\r\n');
+  });
+});
